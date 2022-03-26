e34c0951f1ddff2478ae5f9781f4b1516e900946
abduraxmonjon <boysmile11112005@gmail.com>
2022-03-26 22:34:54 +0300

    darck mode

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const country = document.querySelector('.country')
 const box = document.querySelector('.main-section')
 const searchinput = document.querySelector('.search-input')
+const darcstyle = document.querySelector('.dark')
 
 //api
 const api ='https://restcountries.com/v3.1/all'
@@ -19,9 +20,7 @@ const showContent = (countries) => {
         countryDiv.classList.add('country')
         countryDiv.innerHTML =`
         <a href="#">
-                    
                         <img src=${countri.flags.png} alt="" height="160" width="260">
-                    
                     <h4>
                         ${countri.name.common}
                     </h4>
@@ -41,4 +40,36 @@ const showContent = (countries) => {
     })
 }
 
-
+darcstyle.addEventListener('click', ()=>{
+    document.querySelector('body').style.backgroundColor ='var(--dbg-color)'
+    document.querySelector('.header').style.backgroundColor = 'var(--dark-color)'
+    document.querySelector('h1').style.color = '#fff'
+    document.querySelector('h2').style.color = '#fff'
+    document.querySelector('h2').textContent ='Light Mode'
+    // document.querySelectorAll('.country').style.backgroundColor = 'var(--dark-color)'
+    document.querySelectorAll('.country').forEach(function(el) {
+        el.style.backgroundColor = "var(--dark-color)";
+      })
+      document.querySelectorAll('p').forEach(function(el) {
+        el.style.color = "var(--light-color)";
+      })
+      document.querySelectorAll('span').forEach(function(el) {
+        el.style.color = "var(--light-color)";
+      })
+      document.querySelectorAll('h4').forEach(function(el) {
+        el.style.color = "var(--light-color)";
+      })
+      document.querySelectorAll('.search-input').forEach(function(el) {
+        el.style.backgroundColor = "var(--dark-color)";
+      })
+      document.querySelectorAll('.search-input').forEach(function(el) {
+        el.style.color = "var(--light-color)";
+      })
+      
+      document.querySelectorAll('.country-select').forEach(function(el) {
+        el.style.backgroundColor = "var(--dark-color)";
+    })
+    document.querySelectorAll('.country-select').forEach(function(el) {
+        el.style.color = "var(--light-color)";
+    })
+})
